45c8a64fe187506501b6cab4bc3a65cead771682
scar2o2 <chinnasmart202@gmail.com>
2025-07-28 21:07:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1>
-        Test by ManOfLuck
-      </h1>
     </>
   )
 }
